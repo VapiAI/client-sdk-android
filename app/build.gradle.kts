b3ae59269467b7d1e86ea664a7f49130d28fa7cd
Jordan Dearsley <jordan@dearsley.dev>
2024-08-25 19:41:26 -0700

    Checking in

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     publishing {
@@ -59,7 +59,7 @@ afterEvaluate {
 
                 groupId = "ai.vapi.android"
                 artifactId = "vapi"
-                version = "0.0.1"
+                version = "1.0.0"
 
                 pom {
                     name.set("Vapi")
