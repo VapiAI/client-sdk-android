524a562bbbe27c47d33ef6a9d6ae4bbe245de23b
Jordan Dearsley <jordan@dearsley.dev>
2024-09-11 10:40:13 -0400

    Checking in

--- vapitest/build.gradle.kts
+++ vapitest/build.gradle.kts
@@ -67,5 +67,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation("ai.vapi.android:vapi:1.0.0")
+    implementation("ai.vapi.android:vapi:1.0.1")
 }
